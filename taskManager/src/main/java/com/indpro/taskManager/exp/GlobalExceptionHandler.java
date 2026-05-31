fb98deba4e3067717b77660f517b8f660356286b
sudhirskp <gnitwallah@gmail.com>
2026-05-31 23:01:37 +0530

    Add GlobalExceptionHandler to manage RuntimeExceptions and return error messages

---
+++ taskManager/src/main/java/com/indpro/taskManager/exp/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.indpro.taskManager.exp;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+
+    public Map<String,String> handleRuntimeException(
+            RuntimeException ex){
+
+        return Map.of(
+                "message",
+                ex.getMessage()
+        );
+    }
+}
